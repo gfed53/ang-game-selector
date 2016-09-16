462574538ea15ba799ce72e0db0377ba60cd9e59
gfed53 <gfederico7153@gmail.com>
2016-09-16 14:52:07 -0400

    Added some fundamental grouding/logic in questions section, mock game list

--- partials/questions/questionsController.js
+++ partials/questions/questionsController.js
@@ -5,5 +5,29 @@
 
 	function QuestionsCtrl(){
 		console.log('Questions');
+		var vm = this;
+		// vm.genre
+		//vm.yearRange
+		vm.obj = 
+		vm.add = add;
+		vm.submit = submit;
+
+		var GameChoice = function(genre, yearRange){
+			this.genre = genre;
+			this.yearRange = yearRange;
+		};
+
+		var current = new GameChoice("RPG", [1981,1990]);
+
+		console.log(current);
+
+		function add(key, value){
+
+		}
+
+		function submit(){
+			vm.yearRange = JSON.parse(vm.yearRange);
+			console.log(vm.yearRange);
+		}
 	}
 })();

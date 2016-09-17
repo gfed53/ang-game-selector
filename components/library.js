0edb54ec754d1dfb28533800db094bcc6a600cf8
gfed53 <gfederico7153@gmail.com>
2016-09-17 12:48:41 -0400

    Basic functionality working. Set specs and get back array of matching results

--- components/library.js
+++ components/library.js
@@ -1,5 +1,24 @@
 (function(){
+	angular.module('myApp')
+	.constant('AGS_GAMES_JSON_FILE', './games.json')
+	.factory('agsGames', ['$http', '$q', 'AGS_GAMES_JSON_FILE', agsGames])
 
+	function agsGames($http, $q, AGS_GAMES_JSON_FILE){
+		return function(){
+			var services = {
+				get: get
+			};
 
+			return services;
+
+			function get(){
+				return $http.get(AGS_GAMES_JSON_FILE)
+				.then(function(results){
+					// console.log(results);
+					return $q.when(results);
+				});
+			}
+		}
+	}
 
 })();
--- partials/questions/questionsController.js
+++ partials/questions/questionsController.js
@@ -1,33 +1,56 @@
 (function(){
 	angular.
 	module('myApp')
-	.controller('QuestionsCtrl', [QuestionsCtrl])
+	.controller('QuestionsCtrl', ['agsGames', QuestionsCtrl])
 
-	function QuestionsCtrl(){
-		console.log('Questions');
+	function QuestionsCtrl(agsGames){
 		var vm = this;
 		// vm.genre
 		//vm.yearRange
-		vm.obj = 
-		vm.add = add;
+		vm.obj;
+		vm.set = set;
 		vm.submit = submit;
+		agsGames().get()
+		.then(function(results){
+			vm.results = results.data;
+			console.log(vm.results);
+		});
 
-		var GameChoice = function(genre, yearRange){
+		function GameChoice(genre, yearRange){
 			this.genre = genre;
 			this.yearRange = yearRange;
 		};
 
-		var current = new GameChoice("RPG", [1981,1990]);
+		function Seeker(){
+			this.seek = seek;
 
-		console.log(current);
-
-		function add(key, value){
-
+			function seek(choice, list){
+				console.log(choice);
+				console.log(list);
+				var selection = [];
+				for(var i=0; i<list.length; i++){
+					console.log(list[i]);
+					if(list[i].genre === choice.genre && choice.yearRange[0] <= list[i].releaseYear < choice.yearRange[1]){
+						selection.push(list[i]);
+					}
+				}
+				return selection;
+			}
 		}
 
-		function submit(){
-			vm.yearRange = JSON.parse(vm.yearRange);
-			console.log(vm.yearRange);
+		var current = new GameChoice('RPG', [1981,1990]);
+
+		function set(genre, _yearRange_){
+			yearRange = JSON.parse(_yearRange_);
+			vm.obj = new GameChoice(genre, yearRange);
+			console.log(vm.obj);
+		}
+
+		function submit(genre, _yearRange_){
+			vm.set(genre, _yearRange_);
+			vm.seeker = new Seeker();
+			vm.selection = vm.seeker.seek(vm.obj, vm.results);
+			console.log(vm.selection);
 		}
 	}
 })();
